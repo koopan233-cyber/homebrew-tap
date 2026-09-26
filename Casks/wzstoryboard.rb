1da1d28717ae952b409e909f57df16bd08503284
koo simone <koosimone@192.168.31.112>
2026-09-26 20:46:47 +0800

    wzstoryboard 2.3.2

--- Casks/wzstoryboard.rb
+++ Casks/wzstoryboard.rb
@@ -1,8 +1,8 @@
 cask "wzstoryboard" do
-  version "2.3.1"
-  sha256 "275eccb373aeca733f7bcf4cb64ee54c040444a16d4b25f4053cfb2a81e3d7b4"
+  version "2.3.2"
+  sha256 "1d38f903f6de8b1e08f5aa16d735db6c5e167eb25590e9896d56999cb30600cd"
 
-  url "https://github.com/koopan233-cyber/wzstoryboard-release/releases/download/v2.3.1/wzstoryboard-2.3.1-arm64.dmg"
+  url "https://github.com/koopan233-cyber/wzstoryboard-release/releases/download/v2.3.2/wzstoryboard-2.3.2-arm64.dmg"
   name "大柿编剧故事板"
   desc "编剧故事板工作台"
   homepage "https://wzstoryboard.cc"
